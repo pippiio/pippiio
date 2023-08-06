1ae8c26626fb4704225fa9454b73f0826440fa02
Joachim Rorbol <jr@efio.dk>
2023-08-06 16:26:07 +0200

    Updated to github-org v1.4.0 module

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
 }
 
 module "github" {
-  source = "github.com/pippiio/github-organization?ref=v1.2.0"
+  source = "github.com/pippiio/github-organization?ref=v1.4.0"
 
   organization = merge({ members = {} }, var.organization)
   teams        = var.teams
