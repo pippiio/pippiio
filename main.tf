bdf15c2fb71b6eac6daeb2f9fd2596e165f0dc55
Joachim Rorbol <jr@efio.dk>
2023-08-06 16:27:10 +0200

    Creating techchapter team w. write permissions on all repoes

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
 locals {
   repositories = {
-    for repo_name, repo in var.repositories : repo_name => merge(repo, { team_permission = { "techchapter" = true, "maintaner" = true } })
+    for repo_name, repo in var.repositories : repo_name => merge(repo, {
+      enable_issues      = true
+      enable_discussions = true
+      team_permission = {
+        "techchapter" = true,
+      }
+    })
   }
 }
 
