ab31bb6e8664623bc8922cb214caba406542d323
Joachim Rorbol <jr@efio.dk>
2024-05-22 18:07:32 +0200

    Fixing TechChapter team permissions

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 locals {
   repositories = {
     for repo_name, repo in var.repositories : repo_name => merge(repo, {
-      enable_issues      = true
-      enable_discussions = true
+      enable_issues           = true
+      enable_discussions      = true
+      allow_bypass_protection = true
       team_permission = {
-        "techchapter" = true,
+        "techchapter" = "read_write",
       }
     })
   }
@@ -16,5 +17,4 @@ module "github" {
   organization = merge({ members = {} }, var.organization)
   teams        = var.teams
   repositories = local.repositories
-  environments = var.environments
 }
